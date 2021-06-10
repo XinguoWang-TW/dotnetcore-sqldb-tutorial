5dbdcbca18e49827a5431acf2e9d44f6063fbc55
XinguoWang-TW <xinguo.wang@thoughtworks.com>
2021-06-10 16:32:32 +0800

    configure managed identity

--- Data/MyDatabaseContext.cs
+++ Data/MyDatabaseContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace DotNetCoreSqlDb.Models
@@ -11,9 +12,10 @@ namespace DotNetCoreSqlDb.Models
         public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
             : base(options)
         {
+            var connection = (SqlConnection)Database.GetDbConnection();
+            connection.AccessToken = (new Microsoft.Azure.Services.AppAuthentication.AzureServiceTokenProvider()).GetAccessTokenAsync("https://database.windows.net/").Result;
         }
 
         public DbSet<DotNetCoreSqlDb.Models.Todo> Todo { get; set; }
-        // public DbSet<Customer> Customers { get; set; }
     }
 }
